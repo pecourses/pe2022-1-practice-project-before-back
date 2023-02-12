c7866c03104b637a39fbc82574ea651bcea33c16
Viktoriia <pecourses>
2023-02-12 17:30:53 +0200

    Add firstNamingPlatform

---
+++ client/src/pages/Home/FirstNamingPlatformList/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import firstNamingPlatformCards from './firstNamingPlatform.json';
+import styles from './FirstNamingPlatformList.module.sass';
+
+function FirstNamingPlatformList () {
+  return (
+    <>
+      <div className={styles.namingPlatformListContainer}>
+        <h2>World's #1 Naming Platform</h2>
+        <ul className={styles.namingPlatformList}>
+          {firstNamingPlatformCards.map((c, i) => (
+            <li key={i}>
+              <div className={styles.namingPlatformArticle}>
+                <img src={c.iconSrc} alt={c.title} />
+                <h3>{c.title}</h3>
+                <p>{c.body}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default FirstNamingPlatformList;
